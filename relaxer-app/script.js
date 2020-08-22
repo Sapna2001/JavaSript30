2089b4d90131b8b77e5df13372ead83c2d5fcc2d
Sapna2001 <harisapnanair@gmail.com>
2020-08-22 23:44:03 +0530

    files added

---
+++ relaxer-app/script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector(".container");
+const text = document.querySelector("#text");
+
+const totalTime = 7500
+const breatheTime = (totalTime/5)*2;
+const breatheHold = (totalTime/5);
+
+function breatheAnimation(){
+    console.log("hi")
+  text.innerHTML = "Breathe In";
+  container.className = ("container grow");
+  setTimeout(()=>{
+    text.innerHTML = "Breathe Hold";
+    setTimeout(()=>{
+        text.innerHTML = "Breathe Out";
+        // container.className = ("container shrink")
+        container.classList.add("container","shrink")
+    },breatheHold)
+  },breatheTime)
+}
+
+setInterval(breatheAnimation,totalTime);
