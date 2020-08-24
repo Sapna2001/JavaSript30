d1d605345a5db261f34acd0544677ca6c27a2d82
Sapna2001 <harisapnanair@gmail.com>
2020-08-24 16:07:33 +0530

    files added

---
+++ Event Capture, Propagation, Bubbling and Once/README.md
@@ -0,0 +1,13 @@
+# Event Capture, Propagation,Bubbling and Once
+Features implemented to learn concepts like capture,propagation,bubbling and once.</br>
+[View it](https://capture-bubbling-once-propagation-js.netlify.app/)
+
+## Tech stacks used :-
+- HTML
+- CSS
+- Vanilla JavaScript
+
+## Screenshot :-
+<img src="https://user-images.githubusercontent.com/56690856/91035039-9538e880-e623-11ea-82d6-b24693d69644.png">
+
+
---
+++ Event Capture, Propagation, Bubbling and Once/script.js
@@ -0,0 +1,21 @@
+const divs = document.querySelectorAll("div");
+const button = document.querySelector("button");
+
+function logText(e){
+    console.log(this.classList.value);
+    e.stopPropagation(); //stop bubbling event
+}
+
+divs.forEach(
+    div => div.addEventListener("click",logText,{
+        capture: true,  // top -> bottom (one two three) 
+        // capture: true -> three two one 
+        once: true //onclick unbinds itself which basically means remove 
+    })
+)
+
+button.addEventListener("click",()=>{
+    console.log("click");
+},{once:true})
+// when something is clicked evething it goes from top to bottom and events are captured
+// bubbling -> triggering up the events to the top
