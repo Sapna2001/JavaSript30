fa67ca05796040cbe7feb6c1f5ac4098d89400cb
Sapna2001 <harisapnanair@gmail.com>
2020-08-25 10:12:24 +0530

    commits added

--- Follow Along Link Highlighter/app.js
+++ Follow Along Link Highlighter/app.js
@@ -5,13 +5,14 @@ document.body.appendChild(highlight);
 
 function highlightList (){
   const coords = this.getBoundingClientRect();
+  // The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
   const linkcoords = {
     width: coords.width,
     height: coords.height,
     top: coords.top + window.scrollY,
     left: coords.left + window.scrollX
   };
-  console.log(scrollY);
+  // console.log(height,scrollY);
   highlight.style.width = `${linkcoords.width}px`;
   highlight.style.height = `${linkcoords.height}px`;
   highlight.style.transform = `translate(${linkcoords.left}px, ${linkcoords.top}px)`;
