1152e75d02705b4ae72de8663ae6ecf8ee8629a2
Sapna2001 <harisapnanair@gmail.com>
2020-08-29 11:29:14 +0530

    files added

--- Whack A Mole/README.md
+++ Whack A Mole/README.md
@@ -1,5 +1,9 @@
-# Custom Video Player
+# Whack a Mole
 Whack a mole game</br>
+B=Depending on the last rounds performance level of next game is deceided.</br>
+For 0-4 -> Level 1.</br>
+For 5-9 -> Level 2.</br>
+For >=10 -> Level 3.</br>
 [View it](https://whack-a-mole-js.netlify.app/)
 
 # Teach stacks used :-
--- Whack A Mole/script.js
+++ Whack A Mole/script.js
@@ -1,6 +1,12 @@
 const holes = document.querySelectorAll(".hole");
-const scoreBoard = document.querySelector(".score");
+const scoreBoard = document.querySelector(".scoreshow");
 const moles = document.querySelectorAll(".mole");
+const wrapper = document.querySelector(".wrapper")
+const scoreList = wrapper.querySelector(".score");
+const scores = JSON.parse(localStorage.getItem('scores')) || [];
+const highest = document.querySelector(".highest");
+const level = document.querySelector(".level");
+
 let lastHole;
 let timeUp = false;
 let score = 0;
@@ -20,7 +26,32 @@ function randomHole(holes) {
 	return hole;
 }
 
-function peep() {
+function peep1() {
+	const time = randomTime(300, 1000);
+	const hole = randomHole(holes);
+	hole.classList.add("up");
+    // console.log(time, hole);
+    setTimeout(() => {
+        hole.classList.remove("up");
+        if (!timeUp) //timeup set to true then stops
+        peep1(); 
+        else
+        {
+            const text = score;
+            const currScore ={
+                text
+            }
+            scores.push(currScore);
+            populateList(scores,scoreList);
+            localStorage.setItem('scores', JSON.stringify(scores));
+            if(score>highest.textContent)
+            highest.textContent=score;
+            localStorage.setItem("HScore", highest);
+        }
+    },time)
+}
+
+function peep2() {
 	const time = randomTime(200, 1000);
 	const hole = randomHole(holes);
 	hole.classList.add("up");
@@ -28,15 +59,76 @@ function peep() {
     setTimeout(() => {
         hole.classList.remove("up");
         if (!timeUp) //timeup set to true then stops
-        peep(); 
+        peep2(); 
+        else
+        {
+            const text = score;
+            const currScore ={
+                text
+            }
+            scores.push(currScore);
+            populateList(scores,scoreList);
+            localStorage.setItem('scores', JSON.stringify(scores));
+            if(score>highest.textContent)
+            highest.innerText=score;
+            localStorage.setItem("HScore", highest);
+        }
     },time)
+}
+
+function peep3() {
+	const time = randomTime(100, 1000);
+	const hole = randomHole(holes);
+	hole.classList.add("up");
+    // console.log(time, hole);
+    setTimeout(() => {
+        hole.classList.remove("up");
+        if (!timeUp) //timeup set to true then stops
+        peep3(); 
+        else
+        {
+            const text = score;
+            const currScore ={
+                text,
+            }
+            scores.push(currScore);
+            populateList(scores,scoreList);
+            localStorage.setItem("HScore", highest);
+            localStorage.setItem("CLevel", level.innerText);
+            localStorage.setItem('scores', JSON.stringify(scores));
+            if(score>highest.innerText)
+            highest.innerText=text;
+        }
+    },time)
+}
+
+function populateList(scores,scoreList){
+    scoreList.innerHTML = scores.map((score,index) =>{
+        return `
+            <li class="scoreText">
+                Score ${index+1} = ${score.text}     
+            </li>
+        `
+    }).join("");
 }
 
 function startGame(){
     scoreBoard.innerText = 0;
     timeUp = false;
+    if(score>=0 && score<5)
+    {
+        peep1();
+        level.innerText=1;
+    }
+    if(score>=5 && score<10){
+        peep2();
+        level.innerText=2;
+    }
+    if(score>=10){
+        peep3();
+        level.innerText=3;
+    }
     score=0;
-    peep();
     setTimeout(() => timeUp = true, 10000)
 }
 
@@ -50,3 +142,9 @@ function bonk(e){
 }
 
 moles.forEach((mole) => mole.addEventListener("click",bonk));
+populateList(scores,scoreList);
+if(scores.length !==0){
+    highest.innerText = localStorage.getItem("HScore");
+    level.innerText = localStorage.getItem("CLevel");
+}
+
