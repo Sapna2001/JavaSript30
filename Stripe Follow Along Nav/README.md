7b8f0af4f5943ec502cda8dac444abcf015c22a4
Sapna2001 <harisapnanair@gmail.com>
2020-09-01 14:09:37 +0530

    Files Added

---
+++ Stripe Follow Along Nav/README.md
@@ -0,0 +1,11 @@
+# Follow alond Nav
+Dropdown on hover</br>
+[View it](https://follow-along-nav-js.netlify.app/)
+
+# Teach stacks used :-
+- HTML
+- CSS
+- Vanilla JavaScript
+
+# Sreenshot
+<img src="https://user-images.githubusercontent.com/56690856/91827336-54148a00-ec5c-11ea-9438-f9fd0135785b.png">
---
+++ Stripe Follow Along Nav/script.js
@@ -0,0 +1,37 @@
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
+
+function handleEnter(){
+    // console.log("Enter");
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+        background.classList.add("open");
+        const dropdown = this.querySelector(".dropdown");
+        // console.log(dropdown);
+        const dropdownCoords = dropdown.getBoundingClientRect();
+        const navCoords = nav.getBoundingClientRect();
+        // console.log(dropdownCoords);
+        // console.log(navCoords);
+        const coords = {
+           width:dropdownCoords.width,
+           height:dropdownCoords.height,
+           top:dropdownCoords.top - navCoords.top,
+           left:dropdownCoords.left - navCoords.left,
+        };
+
+        background.style.setProperty("width",`${coords.width}px`);
+        background.style.setProperty("height",`${coords.height}px`);
+        // background.style.setProperty("top",`${coords.top}px`);
+        // background.style.setProperty("left",`${coords.left}px`);
+        background.style.setProperty("transform",`translate(${coords.left}px,${coords.top}px)`);
+}
+
+function handleLeave(){
+    // console.log("Leave"); 
+    this.classList.remove("trigger-enter", "trigger-enter-active"); 
+    background.classList.remove("open");
+}
+
+triggers.forEach(trigger => trigger.addEventListener("mouseenter",handleEnter));
+triggers.forEach(trigger => trigger.addEventListener("mouseleave",handleLeave));
